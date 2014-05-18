732836552483d59fb9f830340379e8714f040f8c
tsheasha <tarek.sheasha@gmail.com>
2014-05-18 20:34:40 +0200

    Keys hidden

--- config/initializers/twitter_client_initializer.rb
+++ config/initializers/twitter_client_initializer.rb
@@ -1,4 +1,4 @@
 TwitterClient = Twitter::REST::Client.new do |config|
-    config.consumer_key= "CFU7FOLB6FXY02qJ46Lhx1R9t" #ENV["TWITTER_API_KEY"]
-    config.consumer_secret = "5SJ6OdLEkoJC2Z2cgL5AFxWEswNPqAMCENyWH7NBl9xQ8fe6Sa" #ENV["TWITTER_API_SECRET"]
+    config.consumer_key= ENV["TWITTER_API_KEY"]
+    config.consumer_secret = ENV["TWITTER_API_SECRET"]
 end
