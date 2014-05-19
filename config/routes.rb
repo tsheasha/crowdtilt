d7f2f5da8c381c40d1424b6e599f51f728c389e1
tsheasha <tarek.sheasha@gmail.com>
2014-05-19 02:40:23 +0200

    Docuxmenting all files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  #Set Route for recents tweets funciton of the TwitterController 
+  #Set Route for recents tweets function of the TwitterController 
   get 'twitter/tweets'
-  #Set Route for common friends funciton of the TwitterController   
+  #Set Route for common friends function of the TwitterController   
   get 'twitter/common_friends'
 
   #Map root url to the index function of the TwitterController
